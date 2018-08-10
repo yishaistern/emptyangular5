ca6cd49b9609010b51aa528cec0c053cc5581586
yishainstern <syishai@cambium.co.il>
2018-08-10 11:36:32 +0300

    d

--- src/app/services/synagogue/synagogue.service.ts
+++ src/app/services/synagogue/synagogue.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Synagogue } from '../../classes/synagogue';
+import { GlobalFunctionsService } from '../global-functions/global-functions.service';
 @Injectable()
 export class SynagogueService {
 
-  constructor() { }
+  constructor(private global: GlobalFunctionsService) { }
 
+  extractComponents(syn: Synagogue, content: any) {
+    
+  }
   editAfield(syn: Synagogue, field: string, contant: any): void {
     switch (field) {
       case 'name':
         syn.setNmae(contant);
+      break;
+      case 'fullAddress':
+        if (contant.formatted_address) {
+          syn.setFullAddress(contant.formatted_address);
+        }
+
       break;
     }
   }
